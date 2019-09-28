ed17732f19aa5669886fad9041c94298a002f388
qs <qs.xiloulou@gmail.com>
2019-09-28 16:12:46 +0800

    - Solved Daily;

---
+++ 26_Remove_Duplicates_From_Sorted_Array/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+Public:
+    int removeDuplicates(vector<int>& nums) {
+        if ( nums.empty() ) return 0;
+
+        int index = 0;
+        for (int i = 0;i < nums.size();i++) {
+            if (nums[index] != nums[i]) {
+                nums[++index] = nums[i];
+            }
+        }
+
+        return index + 1;
+    }
+};
